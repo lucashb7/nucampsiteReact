8e027c68a8c00f274ac0782097198fe720752ad8
Lucas Henrique <lucashenrique@Lucass-MacBook-Pro.local>
2022-07-19 18:52:41 -0700

    campsiteSlice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Hello Nucamp
+      <Header />
+      <CampsitesList/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
