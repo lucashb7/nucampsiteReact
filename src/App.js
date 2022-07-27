cc526d72653ff8caca52ce4c8367db567e49de73
Lucas Henrique <lucashenrique@Lucass-MacBook-Pro.local>
2022-07-27 16:59:46 -0700

    ContactPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
@@ -14,6 +15,7 @@ function App() {
       <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
+          <Route path='about' element={<AboutPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
           <Route
             path='directory/:campsiteId'
